b294b76560931678fb69b90384b70c26ee630e0a
SAMEER TARIQ <imsamtar@gmail.com>
2019-12-13 06:31:20 +0500

    Made changes to Nav.svelte

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,7 +4,7 @@
 
 <nav class="navbar is-primary">
 	<ul class="navbar-menu navbar-end">
-		<li class="navbar-item"><a class="button is-primary" class:has-text-primary='{segment === "login" || segment === "logout"}' href='.'>Signup</a></li>
+		<li class="navbar-item"><a class="button is-primary" class:is-hidden='{segment === 'signup'}' class:has-text-primary='{segment === "logout"}' href='.'>Signup</a></li>
 		<li class="navbar-item"><a class="button is-primary" class:is-hidden='{segment === "login" || segment === "logout"}' href='login'>Login</a></li>
 		<li class="navbar-item"><a class="button is-primary" class:is-hidden='{segment !== "login"}' href='logout'>Logout</a></li>
 		<li class="navbar-item"><a class="button is-primary" target="_blank" href='//github.com/imsamtar/BasicChatApp'><i class="fab fa-github"></i></a></li>
