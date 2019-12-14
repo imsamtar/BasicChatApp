5c2722e3f4079bafb3c1ef2e98b84ba6bed72248
SAMEER TARIQ <imsamtar@gmail.com>
2019-12-14 19:41:09 +0500

    Using dotenv package to read .env file for envirnoment variables

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { PORT, NODE_ENV, dblink } = process.env;
+const dev = NODE_ENV === 'development';
 
 mongoose.connect(process.env.dblink, {
 	useNewUrlParser: true,
@@ -9,9 +15,6 @@ mongoose.connect(process.env.dblink, {
 	useCreateIndex: true,
 	useFindAndModify: false
 }, (err) => console.log(err?err:'Connected to database'));
-
-const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
 
 const app = new express();
 
