e8155444534620da14526b4e411e3ecf62e88cdb
SAMEER TARIQ <imsamtar@gmail.com>
2019-12-15 10:48:18 +0500

    Implemented signup in signup.svelte component

--- src/routes/signup.svelte
+++ src/routes/signup.svelte
@@ -1,28 +1,57 @@
+<script>
+import {goto} from '@sapper/app';
+let user = { username: '', email: '', hash: '', profile: { firstName: '', lastName: ''}};
+async function signup(e){
+	e.preventDefault();
+	if(validate()){
+		console.log(user);
+		let res = await fetch('/api/users', {
+			method: 'POST',
+			body: JSON.stringify({ user }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		if((await res.json()).errmsg!==undefined){
+			alert('Account with this username or email already exists');
+		}else{
+			goto('/login');
+		}
+	}
+}
+function validate(user){
+	return true;
+}
+</script>
+
 <svelte:head>
 	<title>Basic Chat App</title>
 </svelte:head>
 
-<form class="form" on:submit|preventDefault={() => {console.log("Signup")}}>
+<form class="form" on:submit|preventDefault={signup}>
 
-	<label for="fullName" class="label">Full Name:</label>
-	<input type="text" class="input is-primary">
+	<label for="fullName" class="label">First Name:</label>
+	<input type="text" class="input is-primary"  bind:value={user.profile.firstName}>
+
+	<label for="lastName" class="label">Last Name:</label>
+	<input type="text" class="input is-primary"  bind:value={user.profile.lastName}>
 
 	<label for="email" class="label">Email:</label>
 	<div class="control has-icons-left has-icons-right">
-		<input type="email" class="input is-primary email">
+		<input type="email" class="input is-primary email" bind:value={user.email}>
 		<span class="icon is-left"><i class="fas fa-envelope"></i></span>
 		<span class="icon is-right"><i class="fas fa-check"></i></span>
 	</div>
 	<label for="username" class="label">Username:</label>
 	<div class="control has-icons-left has-icons-right">
-		<input type="text" class="input is-primary">
+		<input type="text" class="input is-primary" bind:value={user.username}>
 		<span class="icon is-left is-small"><i class="fas fa-user"></i></span>
 		<span class="icon is-right is-small"><i class="fas fa-check"></i></span>
 	</div>
 
 	<label for="password" class="label">Password:</label>
 	<div class="control has-icons-left has-icons-right">
-		<input type="password" class="input is-primary">
+		<input type="password" class="input is-primary"  bind:value={user.hash}>
 		<span class="icon is-left is-small"><i class="fas fa-key"></i></span>
 		<span class="icon is-right is-small"><i class="fas fa-check"></i></span>
 	</div>
