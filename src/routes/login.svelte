0f31656a072aa7dd575df489b8bc43902ad7da44
SAMEER TARIQ <imsamtar@gmail.com>
2019-12-15 10:48:42 +0500

    Implemented login in login.svelte component

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,19 +1,47 @@
+<script>
+import {goto} from '@sapper/app';
+let user = { username: '', hash: ''};
+async function login(e){
+	e.preventDefault();
+	if(validate()){
+		console.log(user);
+		let res = await fetch('/api/users/auth', {
+			method: 'POST',
+			body: JSON.stringify({ user }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		res = await res.json();
+		console.log(res);
+		if(res.errmsg!==undefined){
+			alert('Username or password incorrect');
+		}else{
+			goto('/chats');
+		}
+	}
+}
+function validate(user){
+	return true;
+}
+</script>
+
 <svelte:head>
 	<title>Login | Basic Chat App</title>
 </svelte:head>
 
-<form class="form" on:submit|preventDefault={() => {console.log("Login")}}>
+<form class="form" on:submit|preventDefault={login}>
 
 	<label for="username" class="label">Username:</label>
 	<div class="control has-icons-left has-icons-right">
-		<input type="text" class="input is-primary">
+		<input type="text" class="input is-primary" bind:value={user.username}>
 		<span class="icon is-left is-small"><i class="fas fa-user"></i></span>
 		<span class="icon is-right is-small"><i class="fas fa-check"></i></span>
 	</div>
 
 	<label for="password" class="label">Password:</label>
 	<div class="control has-icons-left has-icons-right">
-		<input type="password" class="input is-primary">
+		<input type="password" class="input is-primary" bind:value={user.hash}>
 		<span class="icon is-left is-small"><i class="fas fa-key"></i></span>
 		<span class="icon is-right is-small"><i class="fas fa-check"></i></span>
 	</div>
