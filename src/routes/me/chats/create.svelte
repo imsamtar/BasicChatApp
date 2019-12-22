449f562c8b388c095b311d1d9018d75a1dbdc1f1
SAMEER TARIQ <imsamtar@gmail.com>
2019-12-22 13:36:31 +0500

    Can create new chats from frontend

---
+++ src/routes/me/chats/create.svelte
@@ -0,0 +1,94 @@
+<script>
+    import {onMount} from 'svelte';
+    import {goto} from '@sapper/app';
+    import {LS} from '../../../store.js';
+
+    let name = "";
+    let keyword = "";
+    let users = [];
+    let matched = [];
+    onMount(getUsers);
+    async function getUsers(){
+        users = (await (await fetch("/api/users")).json()).reverse();
+        users = users.filter(user => user._id!=LS.user._id);
+    }
+    async function createChat(){
+        let chat = await (await fetch('/api/users/me/chats',{
+            method: 'POST',
+            headers: {
+                "authorization": LS.token,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({name,members: users.filter(u => u.selected).map(u => u.username)})
+        })).json();
+        goto('/me/chats');
+    }
+    $: matched = users.filter(user => user.username.startsWith(keyword));
+</script>
+
+
+
+<style>
+.users {
+    height: 80vh;
+    overflow-y: scroll;
+}
+form {
+    margin-top: 1rem;
+    max-height: 4rem;
+    margin-bottom: 0.3rem;
+}
+form > input {
+    outline: 0;
+    border: 0;
+    display: block;
+    padding: 1rem;
+    background: #eee;
+    color: #555;
+    place-content: center;
+    width: 100%;
+}
+.name {
+    margin-right: 0.3rem;
+}
+.users > * {
+    box-sizing: border-box;
+    margin-bottom: 0.3rem;
+}
+form > input {
+    font-size: 1rem;
+}
+form > button {
+    outline: 0;
+    border: 0;
+    display: inline-block;
+    font-weight: 600;
+    padding: 1rem;
+    place-content: center;
+    color: #777;
+}
+.users > li.selected {
+    border-left: 10px solid black;
+}
+.users > li > div {
+    padding-top: 1rem;
+    height: 4rem;
+    display: block;
+    font-size: 1.3rem;
+    font-weight: 600;
+    color: #555;
+}
+</style>
+
+<form style="display: flex;" on:submit|preventDefault={createChat} >
+    <input bind:value={name} class="name" placeholder="Name: ">
+    <input bind:value={keyword} class="search-username" placeholder="Search by username">
+    <button type="submit"> <i class="fad fa-forward fa-2x"></i> </button>
+</form>
+<ul class="users">
+    {#each matched as user}
+        <li class:selected={user.selected} on:click={() => user.selected = !user.selected}>
+            <div class="list-item notification"><i class="far fa-user-circle fa-lg"></i>&nbsp;&nbsp;{user.username}</div>
+        </li>
+    {/each}
+</ul>
--- src/routes/me/chats/index.svelte
+++ src/routes/me/chats/index.svelte
@@ -3,15 +3,47 @@
     import {LS} from '../../../store.js';
 
     let chats = 0;
-    onMount(async () => {
-        chats = (await (await fetch("/api/users/me/chats", { headers: { "authorization": LS.token } })).json());
-    })
+    onMount(getChats)
+    async function getChats(){
+        chats = (await (await fetch("/api/users/me/chats", { headers: { "authorization": LS.token } })).json()).reverse();
+    }
+    
 </script>
 
+<style>
+.list {
+    margin-top: 1rem;
+    min-height: 70vh;
+}
+.create-button {
+    display: flex;
+    padding: 1rem;
+    background: #eee;
+    color: #555;
+    place-content: center;
+}
+.create-button > i {
+    align-self: center;
+    font-size: 2rem;
+}
+.chats > li {
+    border-bottom: 0.3rem solid #fff;
+}
+.chats > li > a {
+    padding-top: 1rem;
+    height: 4rem;
+    display: block;
+    font-size: 1.3rem;
+    font-weight: 600;
+    color: #555;
+}
+</style>
 
-<ul class="list">
-    <li class="list-item has-text-centered"> <i class="fal fa-plus fa-lg"></i></li>
+<ul class="list chats">
+    <li>
+        <a href="/me/chats/create" class="create-button has-text-centered" > <i class="far fa-plus fa-lg"></i></a>
+    </li>
     {#each chats as chat}
-        <li class="list-item"><a href="/me/chats/{chat._id}"><i class="fal fa-user-circle fa-lg"></i>&nbsp;&nbsp;{chat.name}</a></li>
+        <li><a href="/me/chats/{chat._id}" class="list-item notification"><i class="far fa-user-circle fa-lg"></i>&nbsp;&nbsp;{chat.name}</a></li>
     {/each}
 </ul>
