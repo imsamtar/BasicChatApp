203919951d7d8c804c8c7a0234ae70c49962c3c5
SAMEER TARIQ <imsamtar@gmail.com>
2019-12-29 08:19:12 +0500

    Using aggregate to query db

--- src/routes/api/users/me/chats/[id([a-z0-9]+)]/index.js
+++ src/routes/api/users/me/chats/[id([a-z0-9]+)]/index.js
@@ -1,21 +1,18 @@
 import {User} from '../../../../_database/models/user.js';
 import {Chat} from '../../../../_database/models/chat.js';
 import {Message} from '../../../../_database/models/message.js';
+import {Types} from 'mongoose';
 
 export async function get(req, res){
-    let chat = (await User.findById(req.resp.user._id).populate("chats","_id")).chats.find(c => c._id==req.params.id);
-    if(chat) {
-        chat = (await Chat.findById(chat._id).populate("messages"))["_doc"];
-        for(let i=0;i<chat.messages.length;i++){
-            chat.messages[i];
-            let doc = await User.findById(chat.messages[i].sender).select("username");
-            chat.messages[i] = {...({...chat.messages[i]}['_doc'])};
-            chat.messages[i]['__v']=undefined;
-            chat.messages[i]['sender']=doc.username;
+    try{
+        let chat = (await Chat.aggregate([{$match: {_id: Types.ObjectId(req.params.id) }}, {$unwind: '$messages'}, {$lookup: {from: 'messages', localField: 'messages', foreignField: '_id', as: 'messages'}}, {$unwind: '$messages'}, {$project: {name: 1, 'messages.content': 1, 'messages.sender': 1}}, {$project: {_id: 0}}, {$lookup: {from: 'users', localField: 'messages.sender', foreignField: '_id', as: 'messages.sender'}}, {$project: {'name': 1, 'messages.content': 1, 'messages.sender.username': 1}}, {$unwind: '$messages.sender'}, {$project: {'messages.sender': '$messages.sender.username', 'messages.content': 1, name: 1}}, {$group: {_id: '$name', messages: {$push: '$messages'}}}, {$project: {name: '$_id', _id: 0, messages: 1}}]));
+        if(chat && chat.length>0) {
+            res.json(chat[0])
         }
-        res.json(chat)
+        else res.json({ found: false });
+    }catch(err){
+        res.json({ found: false });
     }
-    else res.json({ found: false });
 }
 
 export async function post(req, res){
